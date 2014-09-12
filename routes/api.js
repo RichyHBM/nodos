c57dc35a584ce7ee9da7f56d3b4084b1e3ae72ad
RichyHBM <gitemail@richyhbm.co.uk>
2014-09-12 14:26:15 +0100

    Started on API

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var sysInfo = require('./../system_info/full-info')
+
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {  ;
+  res.send(sysInfo.getRawInfo());
+});
+
+router.get('/cpu', function(req, res) {
+  res.send(sysInfo.getRawInfo().cpus);
+});
+
+router.get('/network', function(req, res) {
+  res.send(sysInfo.getRawInfo().network);
 });
 
 module.exports = router;
